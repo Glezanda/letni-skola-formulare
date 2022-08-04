8a5bca50765e647d42e9374a78b72ade407df174
Julia Melik <julija.melik@gmail.com>
2022-08-04 11:29:24 +0200

    ikjh

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 let formular = document.querySelector('#formular');
 formular.addEventListener('submit', submit);
 
-function submit(event){
+function submit(event) {
     event.preventDefault()
 
     console.log('Alex Neon is interested in biology and chemistry');
 
     let name = document.querySelector('#jmeno').value;
     let art = document.querySelector('#barva').value;
+
     let contact = document.querySelector('#kontakt').value;
     let number = document.querySelector('#cislo').value;
+
     let date = document.querySelector('#datum').value;
     let box = document.querySelector('#check').checked;
-    console.log(name+ ' ' + art);
-    console.log(contact+ ' ' + number + ' ' + date);
 
-    if(box==true){
+    console.log(name);
+    console.log(art);
+    console.log(contact);
+    console.log(number);
+    console.log(date);
+
+
+    if (box == true) {
         console.log('We appreciate your acception!');
-    }else{
+    } else {
         console.log('Hopefully you agree for the next time.');
     }
 }
