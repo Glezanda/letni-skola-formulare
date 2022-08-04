5bd85ca7a453712ea8f19aba4029363b230da59e
Julia Melik <julija.melik@gmail.com>
2022-08-04 11:06:53 +0200

    oikjn

--- main.js
+++ main.js
@@ -8,8 +8,12 @@ function submit(event){
 
     let name = document.querySelector('#jmeno').value;
     let art = document.querySelector('#barva').value;
+    let contact = document.querySelector('#kontakt').value;
+    let number = document.querySelector('#cislo').value;
+    let date = document.querySelector('#datum').value;
     let box = document.querySelector('#check').checked;
     console.log(name+ ' ' + art);
+    console.log(contact+ ' ' + number + ' ' + date);
 
     if(box==true){
         console.log('We appreciate your acception!');
